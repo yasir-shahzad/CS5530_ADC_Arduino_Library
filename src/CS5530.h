d1f9316826964be4e4de76cdc0f354ea5f94a2eb
yasir-shahzad <Yasirshahzad918@gmail.com>
2022-07-12 03:20:14 +0500

    working

--- src/CS5530.h
+++ src/CS5530.h
@@ -56,63 +56,66 @@
 
 
 //2.3.9. Configuration Register Descriptions
-#define REG_CONFIG_PSS      1UL<<31     //Power Save Select
-#define REG_CONFIG_PDM      1UL<<30     //Power Down Mode
-#define REG_CONFIG_RS       1UL<<29     //Reset System
-#define REG_CONFIG_RV       1UL<<28     //Reset Valid
-#define REG_CONFIG_IS       1UL<<27     //Input Short
-#define REG_CONFIG_VRS      1UL<<25     //Voltage Reference Select
-#define REG_CONFIG_A1       1UL<<24
-#define REG_CONFIG_A0       1UL<<23
-#define REG_CONFIG_FRS      1UL<<19     //Filter Rate Select
-#define REG_CONFIG_UNIPOLAR 1UL<<10     //U/B (Unipolar = 1 / Bipolar = 0) 
-#define REG_CONFIG_OCD      1UL<<9
-
+#define REG_CONFIG_PSS      1UL << 31     //Power Save Select
+#define REG_CONFIG_PDM      1UL << 30     //Power Down Mode
+#define REG_CONFIG_RS       1UL << 29     //Reset System
+#define REG_CONFIG_RV       1UL << 28     //Reset Valid
+#define REG_CONFIG_IS       1UL << 27     //Input Short
+#define REG_CONFIG_VRS      1UL << 25     //Voltage Reference Select
+#define REG_CONFIG_A1       1UL << 24
+#define REG_CONFIG_A0       1UL << 23
+#define REG_CONFIG_FRS      1UL << 19     //Filter Rate Select
+#define REG_CONFIG_UNIPOLAR 1UL << 10     //U/B (Unipolar = 1 / Bipolar = 0) 
+#define REG_CONFIG_OCD      1UL <<  9
 
 
 //CS5532 Gain settings
-#define GAINX1  1UL<<24
-#define GAINX2  2UL<<24
-#define GAINX4  4UL<<24
-#define GAINX8  8UL<<24
-#define GAINX16 16UL<<24
-#define GAINX32 32UL<<24
-#define GAINX64 64UL<<24
-
-//CS5532 Word rate settings 
-#define WORD_RATE_100SPS  0x0<<11
-#define WORD_RATE_50SPS   0x1<<11
-#define WORD_RATE_25SPS   0x2<<11
-#define WORD_RATE_12P5SPS 0x3<<11
-#define WORD_RATE_6P25SPS 0x4<<11
-#define WORD_RATE_3200SPS 0x8<<11
-#define WORD_RATE_1600SPS 0x9<<11
-#define WORD_RATE_800SPS  0xA<<11
-#define WORD_RATE_400SPS  0xB<<11
-#define WORD_RATE_200SPS  0xC<<11
+#define GAINX1              1UL  << 24
+#define GAINX2              2UL  << 24
+#define GAINX4              4UL  << 24
+#define GAINX8              8UL  << 24
+#define GAINX16             16UL << 24
+#define GAINX32             32UL << 24
+#define GAINX64             64UL << 24
 
 
-#define CMD_STOP_CONT_CONV 0xFF
+//Word Rate when FSR=1
+#define WORD_RATE_100SPS    0x0 << 11
+#define WORD_RATE_50SPS     0x1 << 11
+#define WORD_RATE_25SPS     0x2 << 11
+#define WORD_RATE_12P5SPS   0x3 << 11
+#define WORD_RATE_6P25SPS   0x4 << 11
+#define WORD_RATE_3200SPS   0x8 << 11
+#define WORD_RATE_1600SPS   0x9 << 11
+#define WORD_RATE_800SPS    0xA << 11
+#define WORD_RATE_400SPS    0xB << 11
+#define WORD_RATE_200SPS    0xC << 11
+
+//Unipolar / Bipolar
+#define CS5530_UNIPOLAR     1UL << 10
+#define CS5530_BIPOLAR      0UL << 10
+#define REG_DATA_OF         1UL << 3
 
 
-#define CMD_OFFSET_READ  0x09
-#define CMD_OFFSET_WRITE 0x01
-#define CMD_GAIN_READ    0x0A
-#define CMD_GAIN_WRITE   0x02
-#define CMD_CONFIG_READ  0x0B
-#define CMD_CONFIG_WRITE 0x03
+#define CMD_STOP_CONT_CONV  0xFF
 
-#define REG_DATA_OF    1UL << 3
 
-#define CMD_CONVERSION_SIGLE   0x80
-#define CMD_CONVERSION_CONTINU 0xC0
-#define CMD_SYS_OFFSET_CALI    0x85
-#define CMD_SYS_GAIN_CALI      0x86
-#define CMD_SYNC1              0xFF    //Part of the serial port re-initialization sequence.
-#define CMD_SYNC0              0xFE    //End of the serial port re-initialization sequence.
-#define CMD_NULL               0x00    //This command is used to clear a port flag and keep the converter in the continuous conversion mode.
+//Command Register (CS5530_COMM_REG)
+#define CMD_OFFSET_READ         0x09
+#define CMD_OFFSET_WRITE        0x01
+#define CMD_GAIN_READ           0x0A
+#define CMD_GAIN_WRITE          0x02
+#define CMD_CONFIG_READ         0x0B
+#define CMD_CONFIG_WRITE        0x03
 
-#define AFECS 10    // chipselect pin for CS5530
+#define CMD_CONVERSION_SIGLE    0x80
+#define CMD_CONVERSION_CONTINU  0xC0
+#define CMD_SYS_OFFSET_CALI     0x85
+#define CMD_SYS_GAIN_CALI       0x86
+#define CMD_SYNC1               0xFF    //Part of the serial port re-initialization sequence.
+#define CMD_SYNC0               0xFE    //End of the serial port re-initialization sequence.
+#define CMD_NULL                0x00    //This command is used to clear a port flag and keep the converter in the continuous conversion mode.
+
 
 enum EAdStatus {
    E_AD_STATUS_BUSY,
@@ -126,7 +129,6 @@ class CS5530 {
 public:
 
  void spiInit(int);
- 
  u32 twoComplement(u32);
  u8 readByte(void);
  u32 read4Bytes(void);   
